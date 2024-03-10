be6ab4c5de697e9ef8fe364acdd3d5b3808506d2
fhajims <jakob.arneitz@edu.fh-joanneum.at>
2024-03-10 12:38:28 +0100

    Updated npm with npm install --global --production windows-build-tools

--- server/server.js
+++ server/server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path'); // Import the path module
 
+console.log("hallo");
+const sqlite3 = require('sqlite3').verbose();
+
+console.log("hallo");
 const app = express();
 const port = 3000;
+
+
+const db = new sqlite3.Database('kendodatabase.db', (err) => {
+  if (err) {
+      console.error('Error opening database:', err.message);
+  } else {
+      console.log('Connected to the SQLite database.');
+  }
+});
 
 // Middleware
 app.use(bodyParser.json());
@@ -16,6 +29,18 @@ app.get('/', (req, res) => {
   // Instead of sending "Hello World!", send the index.html file
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
+
+app.get('/users', (req, res) => {
+    db.all('SELECT * FROM users', (err, rows) => {
+        if (err) {
+            console.error('Error retrieving data from database:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
 
 app.post('/api/data', (req, res) => {
   const { data } = req.body;
